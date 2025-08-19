26df70d9d2c48ea356dd2180520f100c1ef7a80a
Cedric <cedric.todaro@gmail.com>
2025-08-19 17:07:43 +0200

    Deployed ca9680b with MkDocs version: 1.6.1

---
+++ mathjax.js
@@ -0,0 +1,63 @@
+MathJax = {
+  loader: {
+    load: [
+      "[tex]/autoload",
+      "[tex]/physics",
+      "[tex]/mathtools",
+      "[tex]/ams",
+      "[tex]/cases",
+      "[tex]/textmacros",
+    ],
+  },
+  tex: {
+    packages: [
+      "autoload",
+      "base",
+      "cases",
+      "physics",
+      "ams",
+      "mathtools",
+      "textmacros",
+      "newcommand",
+    ], // extensions to use
+    inlineMath: [
+      ["$", "$"],
+      ["\\(", "\\)"],
+    ],
+    displayMath: [
+      ["$$", "$$"],
+      ["\\[", "\\]"],
+    ],
+    processEscapes: true, // use \$ to produce a literal dollar sign
+    processEnvironments: true, // process \begin{xxx}...\end{xxx} outside math mode
+  },
+  startup: {
+    ready() {
+      MathJax.startup.defaultReady();
+      const { STATE } = MathJax._.core.MathItem;
+      MathJax.tex2mml(String.raw`
+        \let\nc\newcommand
+        \let\rnc\newcommand
+        \let\le\leqslant
+        \let\ge\geqslant
+        \nc{\N}{\mathbb{N}}
+        \nc{\Z}{\mathbb{Z}}
+        \nc{\D}{\mathbb{D}}
+        \nc{\Q}{\mathbb{Q}}
+        \nc{\R}{\mathbb{R}}
+        \nc{\C}{\mathbb{C}}
+        \rnc{\iff}{\Leftrightarrow}
+      `);
+    },
+  },
+  output: {
+    font: "mathjax-schola",
+  },
+};
+
+(function () {
+  var script = document.createElement("script");
+  script.src = "https://cdn.jsdelivr.net/npm/mathjax@4/tex-svg.js";
+  script.defer = true;
+  document.head.appendChild(script);
+})();
